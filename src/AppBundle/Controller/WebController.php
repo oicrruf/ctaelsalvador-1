4582e62f44c290d0c731fc865cc174fc6b9cd621
oicrruf <oicrruf.0286@gmail.com>
2018-01-03 15:33:56 -0600

    Plugins Offline + Página "Equipo Terapéutico"

--- src/AppBundle/Controller/WebController.php
+++ src/AppBundle/Controller/WebController.php
@@ -16,4 +16,19 @@ class WebController extends Controller
         return $this->render('web/index.html.twig');
     }
     
+    /**
+     * @Route("/modelo", name="modelo")
+     */
+    public function modeloAction()
+    {
+        return $this->render('web/modelo.html.twig');
+    }
+    
+    /**
+     * @Route("/equipo", name="equipo_terapeutico")
+     */
+    public function equipoAction()
+    {
+        return $this->render('web/equipo.html.twig');
+    }
 }
